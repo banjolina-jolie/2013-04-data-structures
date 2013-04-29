5abe86d1e4024a91c6b89bc015510b2c7f29faac
Hack Reactor Students <github@hackreactor.com>
2013-04-29 15:03:41 -0700

    copy pasted functional queue into functional-shared queue.

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,1 +1,31 @@
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+var makeQueue = function(){
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var size = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  var queue = {};
+
+  var index = 0;
+
+  queue.add = function(value){
+    storage[size] = value;
+    size += arguments.length;
+  };
+
+  queue.remove = function(){
+    var result = storage[index];
+    delete storage[index];
+    index += 1;
+    size -= 1;
+    return result;
+
+  };
+
+  queue.size = function(){
+    return size;
+  };
+
+  return queue;
+};
