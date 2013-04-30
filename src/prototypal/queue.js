a74ccb520f0022997c31f3b6f337abcebb7f7033
Hack Reactor Students <github@hackreactor.com>
2013-04-29 17:06:28 -0700

    copy pasted from functional shared into prototypal

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,1 +1,34 @@
-// Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var queue = {};
+  queue.storage = {};
+  queue.leng = 0;
+  queue.counter = 0;
+  extend(queue, makeQueue.queueMethods);
+  return queue;
+};
+
+  makeQueue.queueMethods = {};
+
+  makeQueue.queueMethods.add = function(value){
+    this.storage[this.counter] = value;
+    this.counter += 1;
+    this.leng += 1;
+  };
+
+  makeQueue.queueMethods.remove = function(){
+    var result = this.storage[this.counter-this.leng];
+    delete this.storage[this.counter-this.leng];
+    this.leng -= 1;
+    return result;
+
+  };
+
+  makeQueue.queueMethods.size = function(){
+    return this.leng;
+  };
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,1 +1,33 @@
-// Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var makeStack = function(){
+  var stack = {};
+  stack.storage = {};
+  stack.leng = 0;
+  extend(stack, makeStack.stackMethods);
+  return stack;
+};
+
+makeStack.stackMethods = {};
+
+  makeStack.stackMethods.add = function(value){
+    this.storage[this.leng] = value;
+    this.leng += arguments.length;
+  };
+
+  makeStack.stackMethods.remove = function(){
+    var result = this.storage[this.leng -1];
+    delete this.storage[this.leng - 1];
+    this.leng -= 1;
+    return result;
+  };
+
+  makeStack.stackMethods.size = function(){
+    return this.leng;
+  };
+
+
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
