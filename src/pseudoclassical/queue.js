0bc3c25c029242a7fe2bad2fcef1b54d8929a7c0
Hack Reactor Students <github@hackreactor.com>
2013-04-29 17:24:27 -0700

    copy pasted prototypical as a template for pseudoclassical

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,1 +1,25 @@
-// Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  var queue = Object.create(Queue.prototype);
+  queue.storage = {};
+  queue.leng = 0;
+  queue.counter = 0;
+  return queue;
+};
+
+  Queue.prototype.add = function(value){
+    this.storage[this.counter] = value;
+    this.counter += 1;
+    this.leng += 1;
+  };
+
+  Queue.prototype.remove = function(){
+    var result = this.storage[this.counter-this.leng];
+    delete this.storage[this.counter-this.leng];
+    this.leng -= 1;
+    return result;
+
+  };
+
+  Queue.prototype.size = function(){
+    return this.leng;
+  };
--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,1 +1,24 @@
-// Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  var stack = Object.create(Stack.stackMethods);
+  stack.storage = {};
+  stack.leng = 0;
+  return stack;
+};
+
+Stack.stackMethods = {};
+
+  Stack.stackMethods.add = function(value){
+    this.storage[this.leng] = value;
+    this.leng += arguments.length;
+  };
+
+  Stack.stackMethods.remove = function(){
+    var result = this.storage[this.leng -1];
+    delete this.storage[this.leng - 1];
+    this.leng -= 1;
+    return result;
+  };
+
+  Stack.stackMethods.size = function(){
+    return this.leng;
+  };
